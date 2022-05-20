2fa3e76216f88f7fc0b1bbe1612dca9bd5884a0b
nguyendaiky <ngdaiky@gmail.com>
2022-05-21 01:19:32 +0700

    change user model

--- QuanLySTK/admin_site/models.py
+++ QuanLySTK/admin_site/models.py
@@ -9,7 +9,7 @@ from django.db import models
 from django.contrib.auth.models import User, Group
 
 class UsersExtendClass(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     manv = models.CharField(max_length=12)
 
 
