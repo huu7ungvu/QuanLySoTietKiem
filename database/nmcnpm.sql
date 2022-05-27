b1786b10e4e2d69fc15e11a1ea504559c26eacaf
HUU TUNG VU <62176133+huu7ungvu@users.noreply.github.com>
2022-05-27 13:24:48 +0700

    update sql file

--- database/nmcnpm.sql
+++ database/nmcnpm.sql
@@ -14,7 +14,7 @@
 
 use quan_ly_phieu_tiet_kiem;
 
--- Create tables
+# Create tables
 create table KhachHang 
 ( makh varchar(10) not null,
 tenkh varchar(50) not null,
@@ -66,18 +66,19 @@ tongchi decimal(13,2) not null,
 chechlechthuchi decimal(13,2) not null,
 primary key (ngay,maltk)) ;
 
---  Create foreign key
+
+# Create foreign key
 ALTER TABLE Phieutietkiem
 ADD CONSTRAINT FK_PTK_1
-FOREIGN KEY (makh) REFERENCES KhachHang(makh);
+FOREIGN KEY (makh) REFERENCES Khachhang(makh);
 
 ALTER TABLE Phieutietkiem
 ADD CONSTRAINT FK_PTK_2
-FOREIGN KEY (maltk) REFERENCES loaitietkiem(maltk);
+FOREIGN KEY (maltk) REFERENCES Loaitietkiem(maltk);
 
 ALTER TABLE Phieuruttien
 ADD CONSTRAINT FK_PRT_1
-FOREIGN KEY (makh) REFERENCES KhachHang(makh);
+FOREIGN KEY (makh) REFERENCES Khachhang(makh);
 
 ALTER TABLE Phieuruttien
 ADD CONSTRAINT FK_PRT_2
@@ -85,13 +86,13 @@ FOREIGN KEY (maptk) REFERENCES Phieutietkiem(maptk);
 
 ALTER TABLE Baocaothang
 ADD CONSTRAINT FK_BCT
-FOREIGN KEY (maltk) REFERENCES loaitietkiem(maltk);
+FOREIGN KEY (maltk) REFERENCES Loaitietkiem(maltk);
 
 ALTER TABLE Baocaongay
 ADD CONSTRAINT FK_BCN
-FOREIGN KEY (maltk) REFERENCES loaitietkiem(maltk);
+FOREIGN KEY (maltk) REFERENCES Loaitietkiem(maltk);
 
---  Create privilege tables
+# Create privilege tables
 create table Nguoidung 
 ( tendn varchar(50) not null,
 matkhau varchar(50) not null,
@@ -114,13 +115,14 @@ tencn varchar(20) not null,
 tenmanhinhduocloat varchar(20) not null,
 primary key (macn));
 
--- Create thamso table
+# Create thamso table
 create table Thamso
 ( tenthamso varchar(20) not null,
 giatri varchar(10) not null,
 primary key (tenthamso));
 
--- Create foreign key of privilege tables
+
+# Create foreign key of privilege tables
 ALTER TABLE Phanquyen
 ADD CONSTRAINT FK_PQ_1
 FOREIGN KEY (macn) REFERENCES Chucnang(macn);
@@ -129,10 +131,14 @@ ALTER TABLE Phanquyen
 ADD CONSTRAINT FK_PQ_2
 FOREIGN KEY (manhom) REFERENCES Nhomnguoidung(manhom);
 
--- Insert dữ liệu
--- Bảng khách hàng
+-- SHOW VARIABLES LIKE "sql_safe_updates";
+SET SQL_SAFE_UPDATES = 'OFF';
 
+-- Processing --
+show tables;
 
+# Insert dữ liệu
+# Bảng khách hàng
 INSERT INTO KhachHang (makh, tenkh, diachi,cccd) VALUES ('0001','Nguyen Thi Mai Phuong','Nghe An', '012345678');
 INSERT INTO KhachHang (makh, tenkh, diachi,cccd) VALUES ('0002','Tran Van Huynh','Binh Dinh', '013678562');
 INSERT INTO KhachHang (makh, tenkh, diachi,cccd) VALUES ('0003','Nguyen Minh Hieu','Da Nang', '123456789');
@@ -149,49 +155,47 @@ INSERT INTO KhachHang (makh, tenkh, diachi,cccd) VALUES ('0013','Nguyen Thi Huye
 INSERT INTO KhachHang (makh, tenkh, diachi,cccd) VALUES ('0014','Tran Hue Linh','Ca Mau', '012345987');
 INSERT INTO KhachHang (makh, tenkh, diachi,cccd) VALUES ('0015','Pham Van Hung','Tp.HCM', '134798567');
 
-select * from KhachHang;
+select * from khachhang;
 
---  Loại tiết kiệm
-INSERT INTO loaitietkiem(maltk,ltk,kyhan,sotiengoitoithieu,thoigiangoitoithieu,laisuat) VALUES ('LTK01','Khong ky han',0,'100000',15,'0.5');
+# Loại tiết kiệm
+INSERT INTO loaitietkiem(maltk,ltk,kyhan,sotiengoitoithieu,thoigiangoitoithieu,laisuat) VALUES ('LTK01','Khong ky han',0,'100000',15,'0.5'); 
 INSERT INTO loaitietkiem(maltk,ltk,kyhan,sotiengoitoithieu,thoigiangoitoithieu,laisuat) VALUES ('LTK02','3 thang ',3,'100000',90,'5.0');
-INSERT INTO loaitietkiem(maltk,ltk,kyhan,sotiengoitoithieu,thoigiangoitoithieu,laisuat) VALUES ('LTK03','6 thang ',9,'100000',180,'5.5');
+INSERT INTO loaitietkiem(maltk,ltk,kyhan,sotiengoitoithieu,thoigiangoitoithieu,laisuat) VALUES ('LTK03','6 thang ',6,'100000',180,'5.5');
 
--- Phieutietkiem
-INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK001','0001','LTK03','100000000',STR_TO_DATE('01-04-2022', '%d-%m-%Y'),STR_TO_DATE('01-10-2022', '%d-%m-%Y'),'100000000','1');
-INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK002','0002','LTK02','250000000',STR_TO_DATE('10-05-2022', '%d-%m-%Y'),STR_TO_DATE('10-08-2022', '%d-%m-%Y'),'250000000','1');
+# Phieutietkiem
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK001','0001','LTK03','100000000',STR_TO_DATE('01-04-2022', '%d-%m-%Y'),Null,'100000000','1');
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK002','0002','LTK02','250000000',STR_TO_DATE('10-05-2022', '%d-%m-%Y'),Null,'250000000','1');
 INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK003','0003','LTK01','200000000',STR_TO_DATE('09-02-2022', '%d-%m-%Y'),STR_TO_DATE('07-05-2022', '%d-%m-%Y'),'0','0');
-INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK004','0004','LTK02','50000000',STR_TO_DATE('14-01-2022', '%d-%m-%Y'),STR_TO_DATE('14-04-2022', '%d-%m-%Y'),'0','0');
-INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK005','0005','LTK03','150000000',STR_TO_DATE('17-04-2022', '%d-%m-%Y'),STR_TO_DATE('17-10-2022', '%d-%m-%Y'),'150000000','1');
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK004','0004','LTK02','50000000',STR_TO_DATE('14-01-2022', '%d-%m-%Y'),STR_TO_DATE('14-05-2022', '%d-%m-%Y'),'0','0');
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK005','0005','LTK03','150000000',STR_TO_DATE('17-04-2022', '%d-%m-%Y'),Null,'150000000','1');
 INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK006','0006','LTK01','500000000',STR_TO_DATE('06-04-2021', '%d-%m-%Y'),STR_TO_DATE('06-10-2021', '%d-%m-%Y'),'0','0');
-INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK007','0007','LTK02','350000000',STR_TO_DATE('17-07-2021', '%d-%m-%Y'),STR_TO_DATE('17-03-2021', '%d-%m-%Y'),'0','0');
-INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK008','0008','LTK01','600000000',STR_TO_DATE('08-04-2022', '%d-%m-%Y'),STR_TO_DATE('09-10-2022', '%d-%m-%Y'),'400000000','1');
-INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK009','0009','LTK03','100000000',STR_TO_DATE('06-04-2022', '%d-%m-%Y'),STR_TO_DATE('06-10-2022', '%d-%m-%Y'),'100000000','1');
-INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK010','0010','LTK02','780000000',STR_TO_DATE('24-05-2022', '%d-%m-%Y'),STR_TO_DATE('24-08-2022', '%d-%m-%Y'),'100000000','1');
-INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK011','0011','LTK03','100000000',STR_TO_DATE('01-04-2022', '%d-%m-%Y'),STR_TO_DATE('01-10-2022', '%d-%m-%Y'),'780000000','1');
-INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK012','0012','LTK01','800000000',STR_TO_DATE('01-03-2022', '%d-%m-%Y'),STR_TO_DATE('06-10-2022', '%d-%m-%Y'),'500000000','1');
-INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK013','0013','LTK02','130000000',STR_TO_DATE('05-01-2022', '%d-%m-%Y'),STR_TO_DATE('05-04-2022', '%d-%m-%Y'),'0','0');
-INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK014','0001','LTK03','440000000',STR_TO_DATE('11-01-2022', '%d-%m-%Y'),STR_TO_DATE('11-07-2022', '%d-%m-%Y'),'440000000','1');
-INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK015','0001','LTK02','550000000',STR_TO_DATE('23-04-2022', '%d-%m-%Y'),STR_TO_DATE('23-06-2022', '%d-%m-%Y'),'550000000','1');
-INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK016','0001','LTK02','160000000',STR_TO_DATE('21-04-2022', '%d-%m-%Y'),STR_TO_DATE('21-07-2022', '%d-%m-%Y'),'160000000','1');
-INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK017','0002','LTK02','600000000',STR_TO_DATE('09-01-2022', '%d-%m-%Y'),STR_TO_DATE('09-04-2022', '%d-%m-%Y'),'0','0');
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK007','0007','LTK02','350000000',STR_TO_DATE('17-07-2021', '%d-%m-%Y'),STR_TO_DATE('17-11-2021', '%d-%m-%Y'),'0','0');
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK008','0008','LTK01','600000000',STR_TO_DATE('08-04-2022', '%d-%m-%Y'),Null,'600000000','1');
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK009','0009','LTK03','100000000',STR_TO_DATE('06-04-2022', '%d-%m-%Y'),Null,'100000000','1');
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK010','0010','LTK02','780000000',STR_TO_DATE('24-05-2022', '%d-%m-%Y'),Null,'780000000','1');
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK011','0011','LTK03','100000000',STR_TO_DATE('01-04-2022', '%d-%m-%Y'),Null,'100000000','1');
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK012','0012','LTK01','800000000',STR_TO_DATE('01-03-2022', '%d-%m-%Y'),Null,'800000000','1');
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK013','0013','LTK02','130000000',STR_TO_DATE('05-01-2022', '%d-%m-%Y'),STR_TO_DATE('05-05-2022', '%d-%m-%Y'),'0','0');
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK014','0001','LTK03','440000000',STR_TO_DATE('11-01-2022', '%d-%m-%Y'),Null,'440000000','1');
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK015','0001','LTK02','550000000',STR_TO_DATE('23-04-2022', '%d-%m-%Y'),Null,'550000000','1');
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK016','0001','LTK02','160000000',STR_TO_DATE('21-04-2022', '%d-%m-%Y'),Null,'160000000','1');
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK017','0002','LTK02','600000000',STR_TO_DATE('09-01-2022', '%d-%m-%Y'),STR_TO_DATE('09-05-2022', '%d-%m-%Y'),'0','0');
 
-select * from Phieutietkiem;
 
--- Phiếu rút tiền
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK777','0001','LTK02','160000000',STR_TO_DATE('21-02-2022', '%d-%m-%Y'),Null,'160000000','1');
+INSERT INTO Phieutietkiem (maptk,makh,maltk,sotiengoi,ngaymophieu,ngaydongphieu,sodu,tinhtrang) VALUES ('PTK999','0001','LTK01','160000000',STR_TO_DATE('21-01-2022', '%d-%m-%Y'),Null,'160000000','1');
+
+# Phiếu rút tiền
 INSERT INTO Phieuruttien (maprt,makh,maptk,ngayrut,sotienrut) VALUES ('PRT001','0003','PTK003',STR_TO_DATE('07-05-2022', '%d-%m-%Y'),'200000000');
-INSERT INTO Phieuruttien (maprt,makh,maptk,ngayrut,sotienrut) VALUES ('PRT002','0004','PTK004',STR_TO_DATE('14-04-2022', '%d-%m-%Y'),'500000000');
+INSERT INTO Phieuruttien (maprt,makh,maptk,ngayrut,sotienrut) VALUES ('PRT002','0004','PTK004',STR_TO_DATE('14-05-2022', '%d-%m-%Y'),'500000000');
 INSERT INTO Phieuruttien (maprt,makh,maptk,ngayrut,sotienrut) VALUES ('PRT003','0006','PTK006',STR_TO_DATE('06-10-2021', '%d-%m-%Y'),'500000000');
-INSERT INTO Phieuruttien (maprt,makh,maptk,ngayrut,sotienrut) VALUES ('PRT004','0007','PTK007',STR_TO_DATE('17-03-2021', '%d-%m-%Y'),'350000000');
-INSERT INTO Phieuruttien (maprt,makh,maptk,ngayrut,sotienrut) VALUES ('PRT005','0013','PTK013',STR_TO_DATE('04-05-2021', '%d-%m-%Y'),'130000000');
-INSERT INTO Phieuruttien (maprt,makh,maptk,ngayrut,sotienrut) VALUES ('PRT006','0002','PTK017',STR_TO_DATE('04-09-2021', '%d-%m-%Y'),'600000000');
+INSERT INTO Phieuruttien (maprt,makh,maptk,ngayrut,sotienrut) VALUES ('PRT004','0007','PTK007',STR_TO_DATE('17-11-2021', '%d-%m-%Y'),'350000000');
+INSERT INTO Phieuruttien (maprt,makh,maptk,ngayrut,sotienrut) VALUES ('PRT005','0013','PTK013',STR_TO_DATE('05-05-2021', '%d-%m-%Y'),'130000000');
+INSERT INTO Phieuruttien (maprt,makh,maptk,ngayrut,sotienrut) VALUES ('PRT006','0002','PTK017',STR_TO_DATE('09-05-2021', '%d-%m-%Y'),'600000000');
 
--- bangthamso
-Insert into Thamso (tenthamso, giatri) values ('SLNguoiDung','1');
-Insert into Thamso (tenthamso, giatri) values ('SLKhachHang','15');
-Insert into Thamso (tenthamso, giatri) values ('SLPhieuTietKiem','17');
-Insert into Thamso (tenthamso, giatri) values ('SLPhieuRutTien','6');
-
-select * from Thamso;
-
--- SHOW VARIABLES LIKE "sql_safe_updates";
--- SET SQL_SAFE_UPDATES = 'OFF';
+# bangthamso
+Insert into thamso (tenthamso, giatri) values ('SLNguoiDung','1');
+Insert into thamso (tenthamso, giatri) values ('SLKhachHang','15');
+Insert into thamso (tenthamso, giatri) values ('SLPhieuTietKiem','17');
+Insert into thamso (tenthamso, giatri) values ('SLPhieuRutTien','6');
+Insert into thamso (tenthamso, giatri) values ('SLLoaiTietKiem','3');
